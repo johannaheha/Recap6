dc893b384e4a936f0d346c0c0163f09500e1ad7d
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-08-15 13:51:20 +0200

    add config

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         hostname: "images.unsplash.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
